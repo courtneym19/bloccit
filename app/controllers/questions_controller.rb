2f229fdb4129f3a6ab3a0ed09780a0bac7aa8f89
Courtney Miller <courtneymiller919@gmail.com>
2017-05-23 11:21:19 -0600

    assignment 20 partial

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,43 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
+
+    if @question.save
+      flash[:notice] = "Question was saved."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :questions
   resources :posts
 
   get 'about' => 'welcome#about'
