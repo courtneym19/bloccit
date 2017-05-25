24e62d3741005bce8711595f80bb72b3227a5d79
Courtney Miller <courtneymiller919@gmail.com>
2017-05-25 13:21:04 -0600

    assignment 24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
      @user.password_confirmation = params[:user][:password_confirmation]
- 
+
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
        redirect_to root_path
@@ -17,5 +17,13 @@ class UsersController < ApplicationController
        flash.now[:alert] = "There was an error creating your account. Please try again."
        render :new
      end
+   end
+
+   def confirm
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' =>'users#confirm'
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
