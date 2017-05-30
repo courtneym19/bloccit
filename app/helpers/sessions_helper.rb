bd6e204cf040cfd428355f556554e4d06c000f2f
Courtney Miller <courtneymiller919@gmail.com>
2017-05-30 08:02:54 -0600

    assignment 25

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
    def current_user
      User.find_by(id: session[:user_id])
    end
+
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
 end
